4a48db1e99da6c456e79f87eab5db6502d50dee4
Franky Erra <frankyerra43@gmail.com>
2022-04-28 22:03:31 -0700

    Added html and css content; pseudo code for js file

---
+++ script.js
@@ -0,0 +1,7 @@
+// Click event to start quiz. 
+// If click start, page shows a question and possible answers
+// Clicking answer... If true/false, alert messsage to notify user
+// Add to user score
+// current question/answers disappears from page and different Qs and As fill the page
+// repeat line 2-5 n times
+// end page will inlcude 'done' message, user score, text to enter user initials to create highscore list
